6a6a6a962c24eb001c24b1f5b79f3da5663a18cf
Omar Ashraf <Omarlabib.51237@gmail.com>
2024-01-06 18:24:41 +0200

    add the chat gui screen and the controller of it

--- Client/src/main/java/com/example/client/HelloApplication.java
+++ Client/src/main/java/com/example/client/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -9,11 +10,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/hello-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/chat-screen.fxml"));
+        stage.setTitle("Chatting App");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Client!");
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
     }
     public static void main(String[] args) {
         launch();
---
+++ Client/src/main/java/com/example/client/controllers/ChatScreen.java
@@ -0,0 +1,12 @@
+package com.example.client.controllers;
+
+
+import javafx.fxml.FXML;
+
+public class ChatScreen {
+
+    @FXML
+    protected void onHelloButtonClick() {
+        System.out.println("Welcome to JavaFX Application!");
+    }
+}
