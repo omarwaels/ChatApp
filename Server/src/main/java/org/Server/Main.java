fa564710f67c8103e5f28a6ae714bd7e99227375
omarwaels <omarwaelhome@gmail.com>
2024-01-06 09:24:56 +0200

    Ready To Run

---
+++ Server/src/main/java/org/Server/Main.java
@@ -0,0 +1,43 @@
+package org.Server;
+
+
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Sigma
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Button myButton = new Button("Click me!");
+        Label lb = new Label("Server");
+        // Set up a layout pane (StackPane in this case)
+
+        StackPane root = new StackPane();
+        root.getChildren().add(lb);
+        Scene scene = new Scene(root, 300, 250);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
