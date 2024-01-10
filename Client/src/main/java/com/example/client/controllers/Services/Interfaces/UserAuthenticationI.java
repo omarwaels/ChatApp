b21351f314e88dae576c073dcbf5ba237086be24
Mohamed Adel <mohamedadelfarah@gmail.com>
2024-01-10 13:45:11 +0200

    Refactoring the project

--- Client/src/main/java/com/example/client/controllers/Services/Interfaces/UserAuthenticationI.java
+++
@@ -1,5 +0,0 @@
-package com.example.client.controllers.Services.Interfaces;
-
-public interface UserAuthenticationI {
-    public boolean signIn(String phoneNumber, String password);
-}
--- Client/src/main/java/com/example/client/controllers/Services/Interfaces/UserRegisterationI.java
+++
@@ -1,6 +0,0 @@
-package com.example.client.controllers.Services.Interfaces;
-
-public interface UserRegisterationI {
-    public boolean signUp();
-
-}
