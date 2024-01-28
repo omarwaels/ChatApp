b2cecd065e78bdcce22ebd09a436b195ae57c7bd
Mohamed Adel <mohamedadelfarah@gmail.com>
2024-01-28 09:57:25 +0200

    Add Register validation

--- Server/src/main/java/iti/jets/app/server/Launcher.java
+++ Server/src/main/java/iti/jets/app/server/Launcher.java
@@ -1,18 +1,12 @@
 package iti.jets.app.server;
 
-<<<<<<< HEAD
-import iti.jets.app.server.db.UserDao;
-import iti.jets.app.server.models.entities.User;
-=======
 import iti.jets.app.server.Services.ConnectionService;
 import iti.jets.app.shared.DTOs.*;
 import iti.jets.app.shared.Interfaces.client.Client;
->>>>>>> 027b5f55f44c0b031cdeb961a7976d0e461f0ed8
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
@@ -20,19 +14,14 @@ import java.rmi.RemoteException;
 public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
-
-<<<<<<< HEAD
-
-=======
         ConnectionService connectionService = new ConnectionService();
-        ChatScreenDto chatScreenDto =connectionService.connect( new ConnectionDto(new UserLoginDto("01154615235", "omar"), new Client() {
+        ChatScreenDto chatScreenDto = connectionService.connect(new ConnectionDto(new UserLoginDto("01154615235", "omar"), new Client() {
             @Override
             public void recieveMessage(MessageDto messageDto) throws RemoteException {
                 System.out.println("asd");
             }
         }));
         System.out.println(chatScreenDto);
->>>>>>> 027b5f55f44c0b031cdeb961a7976d0e461f0ed8
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("views/server-dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("/iti/jets/app/server/style/style.css").toExternalForm());
@@ -43,25 +32,13 @@ public class Launcher extends Application {
         stage.setWidth(1000);
         stage.setMaxHeight(600);
         stage.setMaxWidth(1000);
-<<<<<<< HEAD
 
 
         System.out.println(Naming.list("rmi://localhost:8090/stub").length);
-
-=======
-        //System.out.println( Naming.list("rmi://localhost:8090/stub" ).length);
->>>>>>> 027b5f55f44c0b031cdeb961a7976d0e461f0ed8
         stage.show();
     }
 
     public static void main(String[] args) {
-        UserDao userDao = new UserDao();
-        User user = userDao.getById("01095944926");
-        if (user == null) {
-            System.out.println("null");
-        } else {
-            System.out.println("not null");
-        }
         launch();
     }
 }
--- Server/src/main/java/iti/jets/app/server/Network/Connect.java
+++ Server/src/main/java/iti/jets/app/server/Network/Connect.java
@@ -1,12 +1,7 @@
 package iti.jets.app.server.Network;
-<<<<<<< HEAD
-
-import iti.jets.app.server.Implementation.ConnectionsImpl;
 import iti.jets.app.server.Implementation.RegisterServiceImpl;
-=======
 import iti.jets.app.server.Services.ConnectionService;
 import iti.jets.app.server.Services.ServerService;
->>>>>>> 027b5f55f44c0b031cdeb961a7976d0e461f0ed8
 import iti.jets.app.shared.Interfaces.server.Connection;
 import iti.jets.app.shared.Interfaces.server.Server;
 
@@ -15,38 +10,19 @@ import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.UnicastRemoteObject;
 
-<<<<<<< HEAD
 
 public class Connect {
-
-
     Registry registry;
-=======
-public class Connect {
-        Registry registry ;
-        public  void openConnection(){
-
-            try {
-                 registry = LocateRegistry.createRegistry(8090 );
-                Connection connection = new ConnectionService();
-                Server server = new ServerService();
-                Naming.rebind("rmi://localhost:8090/connection", connection);
-                Naming.rebind("rmi://localhost:8090/server", server);
-            } catch (RemoteException e) {
-                throw new RuntimeException(e);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Server is open now ....");
->>>>>>> 027b5f55f44c0b031cdeb961a7976d0e461f0ed8
 
     public void openConnection() {
 
         try {
             registry = LocateRegistry.createRegistry(8090);
-            Connection stub = new ConnectionsImpl();
+            Connection connection = new ConnectionService();
+            Server server = new ServerService();
             RegisterServiceImpl registerService = new RegisterServiceImpl();
-            Naming.rebind("rmi://localhost:8090/stub", stub);
+            Naming.rebind("rmi://localhost:8090/connection", connection);
+            Naming.rebind("rmi://localhost:8090/server", server);
             registry.rebind("RegisterService", registerService);
         } catch (RemoteException e) {
             throw new RuntimeException(e);
@@ -54,13 +30,10 @@ public class Connect {
             throw new RuntimeException(e);
         }
         System.out.println("Server is open now ....");
-
-
     }
 
     public void closeConnection() {
         try {
-
             UnicastRemoteObject.unexportObject(registry, true);
             UnicastRemoteObject.unexportObject(registry, false);
             Naming.unbind("rmi://localhost:8090/connection");
@@ -68,9 +41,7 @@ public class Connect {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         System.out.println("Server Closed Successfully ..");
     }
-
-
 }
+
