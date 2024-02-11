a4850f96ae77d7ecece222b51a13dcb1fed36810
Omar Ashraf <Omarlabib.51237@gmail.com>
2024-02-11 09:13:50 +0200

    apply changes to chart
    Co-Authored-By: Maher <109624708+mahernasser@users.noreply.github.com>
    Co-Authored-By: Mohamed Adel <84779586+Mohamed-Adel2@users.noreply.github.com>

--- Server/src/main/java/iti/jets/app/server/fxcontrollers/StatisticsController.java
+++ Server/src/main/java/iti/jets/app/server/fxcontrollers/StatisticsController.java
@@ -1,6 +1,8 @@
 package iti.jets.app.server.fxcontrollers;
 
 import iti.jets.app.server.dashboardservices.implementations.StatisticsServiceImpl;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,6 +14,7 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.Map;
@@ -48,14 +51,41 @@ public class StatisticsController implements Initializable {
 
     @FXML
     private void showGenderChart() {
-        ObservableList<PieChart.Data> list = FXCollections.observableArrayList(
-                new PieChart.Data("Male", StatisticsServiceImpl.getStatisticsService().getMaleFemaleCount().get("Male")),
-                new PieChart.Data("Female", StatisticsServiceImpl.getStatisticsService().getMaleFemaleCount().get("Female"))
-        );
-        pieChart.setTitle("User Gender Statistics");
-        pieChart.setLegendSide(Side.BOTTOM);
-        updateChartMenuButton("Gender");
-        updateChart(list);
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(20), event -> {
+            ObservableList<PieChart.Data> list = FXCollections.observableArrayList(
+                    new PieChart.Data("Male", StatisticsServiceImpl.getStatisticsService().getMaleFemaleCount().get("Male")),
+                    new PieChart.Data("Female", StatisticsServiceImpl.getStatisticsService().getMaleFemaleCount().get("Female"))
+            );
+            pieChart.setTitle("User Gender Statistics");
+            pieChart.setLegendSide(Side.BOTTOM);
+            updateChartMenuButton("Gender");
+            updateChart(list);
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    @FXML
+    private void showUserStatusChart() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(20), event -> {
+            Map<String, Integer> countryCountMap = StatisticsServiceImpl.getStatisticsService().getUsersByStatus();
+            ObservableList<PieChart.Data> list = FXCollections.observableArrayList();
+            countryCountMap.forEach((users, count) -> list.add(new PieChart.Data(users, count)));
+            pieChart.setTitle("User Status Statistics");
+            pieChart.setLegendSide(Side.BOTTOM);
+            updateChartMenuButton("Status");
+            updateChart(list);
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private void updateChart(ObservableList<PieChart.Data> dataList) {
+        pieChart.setData(dataList);
+        for (final PieChart.Data data : pieChart.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, (EventHandler<MouseEvent>) mouseEvent ->
+                    status.setText(String.valueOf((int)data.getPieValue()+" Users")));
+        }
     }
 
     @FXML
@@ -69,27 +99,12 @@ public class StatisticsController implements Initializable {
         updateChart(list);
     }
 
-    @FXML
-    private void showUserStatusChart() {
-        Map<String, Integer> countryCountMap = StatisticsServiceImpl.getStatisticsService().getUsersByStatus();
-        ObservableList<PieChart.Data> list = FXCollections.observableArrayList();
-        countryCountMap.forEach((users, count) -> list.add(new PieChart.Data(users, count)));
-        pieChart.setTitle("User Status Statistics");
-        pieChart.setLegendSide(Side.BOTTOM);
-        updateChartMenuButton("Status");
-        updateChart(list);
-    }
-
-    private void updateChart(ObservableList<PieChart.Data> dataList) {
-        pieChart.setData(dataList);
-        for (final PieChart.Data data : pieChart.getData()) {
-            data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, (EventHandler<MouseEvent>) mouseEvent ->
-                    status.setText(String.valueOf((int)data.getPieValue()+" Users")));
-        }
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(20), event -> updateStats(null)));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
         ObservableList<PieChart.Data> list = FXCollections.observableArrayList(
                 new PieChart.Data("Male", StatisticsServiceImpl.getStatisticsService().getMaleFemaleCount().get("Male")),
                 new PieChart.Data("Female", StatisticsServiceImpl.getStatisticsService().getMaleFemaleCount().get("Female"))
